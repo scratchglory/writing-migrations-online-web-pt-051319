4c011d752e5e00a16892b291b17220a40a44126d
Adella <adellakenn@gmail.com>
2019-07-30 02:16:11 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,11 +1,11 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
-  # def change
-  #   add_column :student do |attribute|
-  #     attribute.string :birthday
-  #     attributes.integer :grade
-  #   end
-  # end
+  def change
+    add_column :student do |attribute|
+      attribute.string :birthday
+      attributes.integer :grade
+    end
+  end
 
   
 end # end of Class
